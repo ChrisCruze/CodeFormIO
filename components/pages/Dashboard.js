86c303c31a19b6fc68d2b8e6e3b4c41ba675f6ba
ChrisCruze <cruzc09@gmail.com>
2020-04-12 14:14:41 -0400

    implemented zapier api post request for code update

--- components/pages/Dashboard.js
+++ components/pages/Dashboard.js
@@ -11,28 +11,104 @@ const codeAirTable = () => {
   });
 };
 
+function upload_file_content_base(name, code) {
+  var formData = new FormData();
+  formData.append("name", name);
+  formData.append("code", code);
+  var blob = new Blob([code], {
+    type: "application/json"
+  });
+  formData.append("webmasterfile", blob);
+  var request = new XMLHttpRequest();
+  request.open("POST", "https://hooks.zapier.com/hooks/catch/229795/lmosoc/");
+  console.log({ formData });
+}
+
+function test_push_implement(name, code) {
+  var formData = new FormData();
+  formData.append("name", name);
+  formData.append("code", code);
+  formData.append("html", code);
+  formData.append("value", code);
+  var blob = new Blob([code], { type: "application/json" });
+  formData.append("webmasterfile", blob);
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = e => {
+    if (request.readyState !== 4) {
+      return;
+    }
+    if (request.status === 200) {
+      console.log("success", request.responseText);
+    } else {
+      console.warn("error");
+    }
+  };
+  console.log({ formData });
+  request.open("POST", "https://hooks.zapier.com/hooks/catch/229795/lmosoc/");
+  request.send(formData);
+}
+
 function upload_file_content(name, code) {
   var formData = new FormData();
   formData.append("name", name);
   formData.append("code", code);
-  var content = code;
-  var blob = new Blob([content], {
+  var blob = new Blob([code], {
     type: "application/json"
   });
   formData.append("webmasterfile", blob);
   var request = new XMLHttpRequest();
   // request.open("POST", "https://hooks.zapier.com/hooks/catch/229795/0u0v92/");
   // request.open("POST", "https://hooks.zapier.com/hooks/catch/229795/csktj2/");
-  console.log("hi");
   request.open("POST", "https://hooks.zapier.com/hooks/catch/229795/lmosoc/");
-  console.log({ formData });
-  // request.send(formData);
+  // console.log({ formData });
+  request.send(formData);
+}
+
+function test_zapier_post() {
+  const data = {
+    code: `function test(){return 1}`,
+    name: "test_function_two",
+    folder: "/Apps/site44/chriscross.site44.com/js",
+    extension: ".js"
+  };
+  // $.ajax({
+  //   type: "POST",
+  //   url: "https://hooks.zapier.com/hooks/catch/229795/o9jn84p/",
+  //   data: data
+  // });
+
+  fetch("https://hooks.zapier.com/hooks/catch/229795/o9jn84p/", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+
+  //   fetch('https://mywebsite.com/endpoint/', {
+  //   method: 'POST',
+  //   headers: {
+  //     Accept: 'application/json',
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     firstParam: 'yourValue',
+  //     secondParam: 'yourOtherValue',
+  //   }),
+  // });
 }
 
 const ButtonClick = e => {
   e.preventDefault();
-  console.log("hi");
-  // upload_file_content("tester123abc", "blah blah");
+  // console.log("hi");
+  test_zapier_post();
+  // const code = `// JavaScript Editing with Firepad!
+  // function go() {
+  //   var message = "Hello, world.";
+  //   console.log(message);
+  // }`;
+  // test_push_implement("eastertesttwo", code);
   // alert("hi");
 };
 const Dashboard = () => {
